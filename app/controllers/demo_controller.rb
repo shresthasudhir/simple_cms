ac831f3f2bb649cb78610416cda7a2f2fe894e64
shresthasudhir <shresthasudhir009@gmail.com>
2017-05-22 16:04:21 -0500

    simple cms commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,26 @@
+class DemoController < ApplicationController
+
+	layout 'application'
+
+  def index
+  	#render('hello')
+  	#render(:template => 'demo/hello')
+  end
+
+  def hello
+  	#render('index')
+  	@array = [1,2,3,4,5]
+  	@id = params['id'].to_i
+  	@page = params[:page].to_i
+  end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def google
+  	redirect_to("http://google.com")
+  end
+
+
+end
