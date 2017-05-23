5a0e0b566a3cf1cb1cdacf6632f102415a5f6e8f
shresthasudhir <shresthasudhir009@gmail.com>
2017-05-22 22:10:14 -0500

    user authentication complete

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,8 @@ class AccessController < ApplicationController
 
   layout 'admin'
 
+  before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
+
   def index
   	#display text & links
 
@@ -20,7 +22,9 @@ class AccessController < ApplicationController
       end
     end
     if authorized_user
-      # TODO: mark user as logged in
+      # mark user as logged in
+      session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
       flash[:notice] = "You are now logged in."
       redirect_to(:action => 'index')
     else
@@ -30,7 +34,9 @@ class AccessController < ApplicationController
   end
 
   def logout
-  	# TODO: mark user as logged out
+  	# mark user as logged out
+    session[:user_id] = nil
+      session[:username] = nil
     flash[:notice] = "Logged out"
     redirect_to(:action => "login")
   end
--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,6 +2,8 @@ class DemoController < ApplicationController
 
 	layout 'application'
 
+  before_action :confirm_logged_in
+
   def index
   	#render('hello')
   	#render(:template => 'demo/hello')
